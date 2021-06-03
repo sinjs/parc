aca5c5694b8dfca540d9a0dca1bec9a06d142425
sinmineryt <50749166+sinmineryt@users.noreply.github.com>
2021-06-04 00:10:47 +0200

    Util: Add timer

---
+++ parc/util/timer.py
@@ -0,0 +1,24 @@
+import time
+
+
+class TimerError(Exception):
+    """TimerError"""
+
+
+class Timer:
+    def __init__(self):
+        self._start_time = None
+
+    def start(self):
+        if self._start_time is not None:
+            raise TimerError(f"Timer is running. Use .stop() to stop it")
+        self._start_time = time.perf_counter()
+
+    def stop(self, print_time=True):
+        if self._start_time is None:
+            raise TimerError(f"Timer is not running. Use .start() to start it")
+        elapsed_time = time.perf_counter() - self._start_time
+        self._start_time = None
+        if print_time:
+            print(f"Elapsed time: {elapsed_time:0.4f} seconds")
+        return elapsed_time
