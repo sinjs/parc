381bdc1533cd2f07981a8757f43860c0daf49b2c
sinmineryt <50749166+sinmineryt@users.noreply.github.com>
2021-06-04 13:26:09 +0200

    CLI: Add version flag

--- parc/cli.py
+++ parc/cli.py
@@ -1,10 +1,15 @@
 import multicommand
 from parc import parsers
+from parc.version import __version__
 
 
 def cli():
     parser = multicommand.create_parser(parsers)
+    parser.add_argument("-v", "--version", help="display version and exit", action="store_true")
     args = parser.parse_args()
+    if args.version:
+        print(f"parc {__version__['number']} {__version__['type']}")
+        return
     if hasattr(args, "handler"):
         args.handler(args)
         return
